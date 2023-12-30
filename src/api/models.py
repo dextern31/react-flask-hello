c70a8bf4594aebaec0bf5c78f0d6711ef4b041fc
dextern31 <140563233+dextern31@users.noreply.github.com>
2023-12-30 00:39:03 +0000

    Pending changes exported from your codespace

--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    firstName = db.Column(db.String)
+    lastName = db.Column(db.String)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +18,60 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "firstName": self.firstName,
+            "lastName": self.lastName,
             # do not serialize the password, its a security breach
         }
+    
+class Post(db.Model):
+    __tablename__ = 'post'
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String, nullable=False)
+    stay = db.Column(db.String, nullable=False)
+    foodDrinks = db.Column(db.String, nullable=False)
+    activities = db.Column(db.String, nullable=False)
+    transportation = db.Column(db.String, nullable=False)
+    tips = db.Column(db.String, nullable=False)
+    userID = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    user = db.relationship("User")
+
+    def serialize(self):
+        #comments = list(map(lambda x: x.serialize(), self.user))
+        return {
+            "id": self.id,
+            "location": self.location,
+            "stay": self.stay,
+            "foodDrinks": self.foodDrinks,
+            "activities": self.activities,
+            "transportation": self.transportation,
+            "tips": self.tips,
+            "userID": self.userID
+        }
+    
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    comment = db.Column(db.String)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete='CASCADE'), nullable=False)
+    post = db.relationship("Post")
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    user = db.relationship("User")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "comment": self.comment,
+            "post_id": self.post_id,
+            "user_id": self.user_id
+        }
+
+class Friends(db.Model):
+    __tablename__ = 'friends'
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True, nullable=False)
+    friend_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True, nullable=False)
+    user = db.relationship("User", foreign_keys=[user_id])
+    friends = db.relationship("User", foreign_keys=[friend_id])
+
+
+    
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Post, Comment
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -20,3 +20,28 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/users', methods=['GET'])
+def getUsers():
+    users = User.query.all()
+    allUsers = list(map(lambda x: x.serialize(), users))
+
+    return jsonify(allUsers), 200
+
+@api.route('/posts', methods=['GET'])
+def getPosts():
+    posts = Post.query.all()
+    allPosts = list(map(lambda x: x.serialize(), posts))
+
+    return jsonify(allPosts), 200
+
+@api.route('/comments', methods=['GET'])
+def getComments():
+    comments = Comment.query.all()
+    allComments = list(map(lambda x: x.serialize(), comments))
+
+    return jsonify(allComments), 200
+
+@api.route('/signup', methods=['POST'])
+def createUser():
+    return
